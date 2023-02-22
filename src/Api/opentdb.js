6092257320da898a4d44fbef99fa5c2dee685ee1
elijahd123 <elijahrdeal@gmail.com>
2023-02-22 13:02:26 +0000

    uhhhhhh

--- src/Api/opentdb.js
+++ src/Api/opentdb.js
@@ -1,4 +1,12 @@
-export const getMultipleChoice = async () => {
-  const response = await axios(`https://opentdb.com/api.php?amount=10&type=multiple`)
+const getQuestions = async (type) => {
+  const response = await axios(`https://opentdb.com/api.php?amount=10&type=${type}`)
   return response.data.results;
+}
+
+export const getMultipleChoice = async () => {
+  return getQuestions("multiple")
+}
+
+export const getTrueFalse = async () => {
+  return getQuestions("boolean")
 }
